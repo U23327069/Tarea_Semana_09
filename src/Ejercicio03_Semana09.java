ade1bf4f175701d34fb2bc7d3588dec05455c7bb
miguel.coveñas <miguel.coveas@PYT08.smartglobal.pe>
2024-10-19 14:29:07 -0500

    Desarrollo de la semana 09.

--- src/Ejercicio03_Semana09.java
+++ src/Ejercicio03_Semana09.java
@@ -2,20 +2,18 @@ import java.util.Scanner;
 public class Ejercicio03_Semana09 {
         public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);        
-        //Solicitar el número de personas
+        //Declaramos las variables
         System.out.print("Ingrese el numero de personas: ");
         int n = lectura.nextInt();        
         short[] compras = new short[n];        
         int totalCompras = 0;
         short compraMayor = Short.MIN_VALUE;
         short compraMenor = Short.MAX_VALUE;        
-        //Ingresar las compras de las personas
+        //Ingresamos las compras
         for (int i = 0; i < n; i++) {
             System.out.print("Ingrese el valor de la compra de la persona " + (i + 1) + ": ");
-            compras[i] = lectura.nextShort();            
-            // Sumar la compra actual al total
-            totalCompras += compras[i];            
-            // Verificar si es la mayor compra
+            compras[i] = lectura.nextShort();
+            totalCompras += compras[i];
             if (compras[i] > compraMayor) {
                 compraMayor = compras[i];
             }            
